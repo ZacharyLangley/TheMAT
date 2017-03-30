757a316085df4b9f456bf1bf6503bc2dced396d2
Joseph Vargas <vargman54@gmail.com>
2017-03-29 20:55:24 -0500

    Added Profiles/Profile Pages/DB access/DB data

--- themat/scheduler/urls.py
+++ themat/scheduler/urls.py
@@ -7,5 +7,7 @@ from . import views
 urlpatterns = [
     #Index
     url(r'^$', views.index, name='index'),
-    url(r'^profile/$', views.profile, name='profile'),
+    url(r'^profiles/$', views.profileList, name='profiles'),
+    url(r'^profiles/(?P<venue_id>[0-9]+)$', views.profile, name="profile"),
+
 ]
--- themat/scheduler/views.py
+++ themat/scheduler/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render, redirect
+from .models import Venue
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
-def profile(request):
-    return render(request, 'profile.html')
+def profileList(request):
+    venues = Venue.objects.all()
+    context = {'venues': venues}
+    return render(request, 'profileList.html', context)
+
+def profile(request, venue_id):
+    venue = Venue.objects.filter(id=venue_id)
+    context = {'venue': venue}
+    print(venue)
+    return render(request, "profile.html", context)
